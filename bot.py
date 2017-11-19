9f2194f684373238e16597c5d4c42a9b70b15a17
Strudy <proteximportsexports@gmail.com>
2017-11-19 18:31:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,4 +3,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 client = discord_cfg.Client("ex.cfg")
 client.run()
-exit()
+permissions = discord_cfg.permissions("per.cfg")
+permissions.run()
+
